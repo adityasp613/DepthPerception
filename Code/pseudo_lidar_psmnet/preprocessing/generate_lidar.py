a09c9ed093f4b9b69507d66b94e5c45facaada2a
Manasi Purohit <manasipurohit8@gmail.com>
2022-03-12 14:58:28 -0500

    Making changes to generate lidar

--- Code/pseudo_lidar_psmnet/preprocessing/generate_lidar.py
+++ Code/pseudo_lidar_psmnet/preprocessing/generate_lidar.py
@@ -6,6 +6,8 @@ import scipy.misc as ssc
 
 import kitti_util
 
+import matplotlib.pyplot as plt
+
 
 def project_disp_to_points(calib, disp, max_high):
     disp[disp < 0] = 0
@@ -28,9 +30,10 @@ def project_depth_to_points(calib, depth, max_high):
     points = np.stack([c, r, depth])
     points = points.reshape((3, -1))
     points = points.T
-    cloud = calib.project_image_to_velo(points)
+    pseudo_cloud_rect = calib.project_image_to_rect(points)
+    cloud = calib.project_rect_to_velo(pseudo_cloud_rect)
     valid = (cloud[:, 0] >= 0) & (cloud[:, 2] < max_high)
-    return cloud[valid]
+    return pseudo_cloud_rect, cloud[valid]
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate Libar')
@@ -40,8 +43,11 @@ if __name__ == '__main__':
                         default='~/Kitti/object/training/predicted_disparity')
     parser.add_argument('--save_dir', type=str,
                         default='~/Kitti/object/training/predicted_velodyne')
+    parser.add_argument('--save_plot_dir', type=str,
+                        default='~/Kitti/object/training/pseudo_lidar_plot')
     parser.add_argument('--max_high', type=int, default=1)
     parser.add_argument('--is_depth', action='store_true')
+    parser.add_argument('--save_plot', type=bool, default=False)
 
     args = parser.parse_args()
 
@@ -70,9 +76,26 @@ if __name__ == '__main__':
             lidar = project_disp_to_points(calib, disp_map, args.max_high)
         else:
             disp_map = (disp_map).astype(np.float32)/256.
-            lidar = project_depth_to_points(calib, disp_map, args.max_high)
+            pseudo_lidar, lidar = project_depth_to_points(calib, disp_map, args.max_high)
         # pad 1 in the indensity dimension
         lidar = np.concatenate([lidar, np.ones((lidar.shape[0], 1))], 1)
         lidar = lidar.astype(np.float32)
-        lidar.tofile('{}/{}.bin'.format(args.save_dir, predix))
+        # lidar.tofile('{}/{}.bin'.format(args.save_dir, predix))
+        # print('Finish Depth {}'.format(predix))
+
+        #3D scatter plot of pseudo lidar point cloud
+        if args.save_plot:
+          fig = plt.figure()
+          ax = fig.add_subplot(projection='3d')
+
+          xs = pseudo_lidar[:, 0] * 255
+          ys = pseudo_lidar[:, 1] * 255
+          zs = pseudo_lidar[:, 2] * 255
+
+          ax.scatter(xs, ys, zs)
+          pseudo_lidar = np.reshape(pseudo_lidar, (disp_map.shape[0], disp_map.shape[1], 3))
+          fig.savefig('{}/{}.png'.format(args.save_plot_dir, predix))
+
+        np.save('{}/{}.npy'.format(args.save_dir, predix), pseudo_lidar)
         print('Finish Depth {}'.format(predix))
+
