eabd9ace53c2e823ec9f9dde742d0bc7c901c14f
Ubuntu <ubuntu@ip-172-31-40-251.us-east-2.compute.internal>
2022-03-12 21:01:41 +0000

    temp

--- saved_copy/midas_experiment.py
+++ saved_copy/midas_experiment.py
@@ -1,3 +1,4 @@
+import numpy as np
 import cv2
 import torch
 import urllib
@@ -5,7 +6,9 @@ import matplotlib.pyplot as plt
 
 #url, filename = ("https://github.com/pytorch/hub/raw/master/images/dog.jpg", "dog.jpg")
 #urllib.request.urlretrieve(url, filename)
-filename = "/home/ubuntu/Pictures/image_222.jpg"
+filename = "/home/ubuntu/18744/DepthPerception/saved_copy/carla_images/image_224.jpg"
+data_filename = "/home/ubuntu/18744/DepthPerception/saved_copy/calib.npy"
+K = np.load(data_filename)
 model_type = "DPT_Large"
 midas = torch.hub.load("intel-isl/MiDaS", model_type)
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
@@ -19,6 +22,7 @@ else:
     transform = midas_transforms.small_transform
 
 img = cv2.imread(filename)
+print(img)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 input_batch = transform(img).to(device)
@@ -32,9 +36,48 @@ with torch.no_grad():
         align_corners=False,
     ).squeeze()
 
-output = prediction.cpu().numpy()
-plt.subplot(211)
-plt.imshow(img)
-plt.subplot(212)
-plt.imshow(output)
-plt.show()
+
+depth = prediction.cpu().numpy()
+
+print(depth.shape)
+rows, cols = depth.shape
+c, r = np.meshgrid(np.arange(cols), np.arange(rows))
+points = np.stack([c, r, depth])
+points = points.reshape((3, -1))
+points = points.T
+f_u = K[0, 0]
+f_v = K[1, 1]
+c_u =  K[0, 2]
+c_v =  K[1, 2]
+b_x = 0
+b_y = 0
+def project_image_to_rect(uv_depth):
+        ''' Input: nx3 first two channels are uv, 3rd channel
+                   is depth in rect camera coord.
+            Output: nx3 points in rect camera coord.
+        '''
+        n = uv_depth.shape[0]
+        x = ((uv_depth[:, 0] - c_u) * uv_depth[:, 2]) / f_u + b_x
+        y = ((uv_depth[:, 1] - c_v) * uv_depth[:, 2]) / f_v + b_y
+        pts_3d_rect = np.zeros((n, 3))
+        pts_3d_rect[:, 0] = x
+        pts_3d_rect[:, 1] = y
+        pts_3d_rect[:, 2] = uv_depth[:, 2]
+        return pts_3d_rect
+
+pseudo_lidar = project_image_to_rect(points)
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+xs = pseudo_lidar[:, 0] * 255
+ys = pseudo_lidar[:, 1] * 255
+zs = pseudo_lidar[:, 2] * 255
+
+np.savez("3d_data", xs = xs, ys = ys, zs = zs)
+#ax.scatter(xs, ys, zs)
+# plt.subplot(211)
+# plt.imshow(img)
+# plt.subplot(212)
+# plt.imshow(output)
+# plt.show()
