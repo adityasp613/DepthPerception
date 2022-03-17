f02dd191df325a66463cf8cca462e857e86b82e5
Manasi Purohit <manasipurohit8@gmail.com>
2022-03-17 15:25:49 -0400

    Adding integrated code

--- Code/packnet-sfm/scripts/infer.py
+++ Code/packnet-sfm/scripts/infer.py
@@ -7,7 +7,7 @@ import torch
 
 from glob import glob
 from cv2 import imwrite
-
+import matplotlib.pyplot as plt
 import sys
 sys.path.append("/content/DepthPerception/Code/packnet-sfm/")
 
@@ -20,6 +20,39 @@ from packnet_sfm.utils.load import set_debug
 from packnet_sfm.utils.depth import write_depth, inv2depth, viz_inv_depth
 from packnet_sfm.utils.logging import pcolor
 
+#######################################################################
+import kitti_util
+import scipy.misc as ssc
+#######################################################################
+
+
+#######################################################################
+def project_disp_to_points(calib, disp, max_high):
+    disp[disp < 0] = 0
+    baseline = 0.54
+    mask = disp > 0
+    depth = calib.f_u * baseline / (disp + 1. - mask)
+    rows, cols = depth.shape
+    c, r = np.meshgrid(np.arange(cols), np.arange(rows))
+    points = np.stack([c, r, depth])
+    points = points.reshape((3, -1))
+    points = points.T
+    points = points[mask.reshape(-1)]
+    cloud = calib.project_image_to_velo(points)
+    valid = (cloud[:, 0] >= 0) & (cloud[:, 2] < max_high)
+    return cloud[valid]
+
+def project_depth_to_points(calib, depth, max_high):
+    rows, cols = depth.shape
+    c, r = np.meshgrid(np.arange(cols), np.arange(rows))
+    points = np.stack([c, r, depth])
+    points = points.reshape((3, -1))
+    points = points.T
+    pseudo_cloud_rect = calib.project_image_to_rect(points)
+    cloud = calib.project_rect_to_velo(pseudo_cloud_rect)
+    valid = (cloud[:, 0] >= 0) & (cloud[:, 2] < max_high)
+    return pseudo_cloud_rect, cloud[valid]
+#######################################################################
 
 def is_image(file, ext=('.png', '.jpg',)):
     """Check if a file is an image with certain extensions"""
@@ -37,6 +70,19 @@ def parse_args():
     parser.add_argument('--half', action="store_true", help='Use half precision (fp16)')
     parser.add_argument('--save', type=str, choices=['npz', 'png', 'npy'], default=None,
                         help='Save format (npz or png or npy). Default is None (no depth map is saved).')
+    ##############################################################
+    parser.add_argument('--calib_dir', type=str,
+                        default='~/Kitti/object/training/calib')
+    parser.add_argument('--disparity_dir', type=str,
+                        default='~/Kitti/object/training/predicted_disparity')
+    parser.add_argument('--save_dir', type=str,
+                        default='~/Kitti/object/training/predicted_velodyne')
+    parser.add_argument('--save_plot_dir', type=str,
+                        default='~/Kitti/object/training/pseudo_lidar_plot')
+    parser.add_argument('--max_high', type=int, default=1)
+    parser.add_argument('--is_depth', action='store_true')
+    parser.add_argument('--save_plot', type=bool, default=False)
+    ###############################################################
     args = parser.parse_args()
     assert args.checkpoint.endswith('.ckpt'), \
         'You need to provide a .ckpt file as checkpoint'
@@ -45,6 +91,13 @@ def parse_args():
     assert (is_image(args.input) and is_image(args.output)) or \
            (not is_image(args.input) and not is_image(args.input)), \
         'Input and output must both be images or folders'
+    ################################################################
+    # assert os.path.isdir(args.disparity_dir)
+    assert os.path.isdir(args.calib_dir)
+
+    if not os.path.isdir(args.save_dir):
+        os.makedirs(args.save_dir)
+    #################################################################
     return args
 
 
@@ -95,7 +148,9 @@ def infer_and_save_depth(input_file, output_file, model_wrapper, image_shape, ha
         print('Saving {} to {}'.format(
             pcolor(input_file, 'cyan', attrs=['bold']),
             pcolor(filename, 'magenta', attrs=['bold'])))
+        depth_npy = inv2depth(pred_inv_depth)
         write_depth(filename, depth=inv2depth(pred_inv_depth))
+        return depth_npy
     else:
         # Prepare RGB image
         rgb = image[0].permute(1, 2, 0).detach().cpu().numpy() * 255
@@ -108,6 +163,7 @@ def infer_and_save_depth(input_file, output_file, model_wrapper, image_shape, ha
             pcolor(input_file, 'cyan', attrs=['bold']),
             pcolor(output_file, 'magenta', attrs=['bold'])))
         imwrite(output_file, image[:, :, ::-1])
+        return image[:, :, ::-1]
 
 
 def main(args):
@@ -154,8 +210,55 @@ def main(args):
 
     # Process each file
     for fn in files[rank()::world_size()]:
-        infer_and_save_depth(
+        depth = infer_and_save_depth(
             fn, args.output, model_wrapper, image_shape, args.half, args.save)
+        
+        # disps = [x for x in os.listdir(args.disparity_dir) if x[-3:] == 'png' or x[-3:] == 'npy']
+        # disps = sorted(disps)
+        ###############################################################################
+
+        #predix = fn[:-4]
+        predix = os.path.split(fn)[-1].split('.')[0]
+        print(args.calib_dir, os.path.split(fn)[-1].split('.')[0])
+        calib_file = '{}/{}.txt'.format(args.calib_dir, predix)
+        calib = kitti_util.Calibration(calib_file)
+        # disp_map = ssc.imread(args.disparity_dir + '/' + fn) / 256.
+        # if fn[-3:] == 'png':
+        #     disp_map = ssc.imread(args.disparity_dir + '/' + fn)
+        # elif fn[-3:] == 'npy':
+        #     disp_map = np.load(args.disparity_dir + '/' + fn)
+        # else:
+        #     assert False
+        # if not args.is_depth:
+        #     disp_map = (disp_map*256).astype(np.uint16)/256.
+        #     lidar = project_disp_to_points(calib, disp_map, args.max_high)
+        # else:
+        if torch.is_tensor(depth):
+          depth = depth.detach().squeeze().cpu().numpy()
+        disp_map = (depth).astype(np.float32)/256.
+        pseudo_lidar, lidar = project_depth_to_points(calib, disp_map, args.max_high)
+        # pad 1 in the indensity dimension
+        lidar = np.concatenate([lidar, np.ones((lidar.shape[0], 1))], 1)
+        lidar = lidar.astype(np.float32)
+        # lidar.tofile('{}/{}.bin'.format(args.save_dir, predix))
+        # print('Finish Depth {}'.format(predix))
+
+        #3D scatter plot of pseudo lidar point cloud
+        if args.save_plot:
+          fig = plt.figure()
+          ax = fig.add_subplot(projection='3d')
+
+          xs = pseudo_lidar[:, 0] * 255
+          ys = pseudo_lidar[:, 1] * 255
+          zs = pseudo_lidar[:, 2] * 255
+
+          ax.scatter(xs, ys, zs)
+          pseudo_lidar = np.reshape(pseudo_lidar, (disp_map.shape[0], disp_map.shape[1], 3))
+          fig.savefig('{}/{}.png'.format(args.save_plot_dir, predix))
+
+        np.save('{}/{}.npy'.format(args.save_dir, predix), pseudo_lidar)
+        print('Finish Depth {}'.format(predix))
+    ###############################################################################
 
 
 if __name__ == '__main__':
